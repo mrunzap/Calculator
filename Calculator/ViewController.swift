bf22fad1b33ce663eb2dfcdf8da5b969f7d5ae90
mrunzap <mrunzap@gmail.com>
2022-10-03 22:26:39 +0900

    1. 연산추가

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -47,6 +47,55 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func addButton(_ sender: UIButton) {
+        self.operation(operation: .Add)
+    }
+    
+    @IBAction func multiplyButton(_ sender: UIButton) {
+        self.operation(operation: .Multiply)
+    }
+    
+    @IBAction func substractButton(_ sender: UIButton) {
+        self.operation(operation: .Substract)
+    }
+    
+    @IBAction func divideButton(_ sender: UIButton) {
+        self.operation(operation: .Divide)
+    }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+        self.operation(operation: self.currentOperation)
+    }
+    
+    private func operation(operation : Operation) {
+        // 이미 연산자를 클릭했다면, 첫번째 피연산자 값이 있다는 것
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.sencondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                //소수값 일수 있으니 Double타입으로 형변환
+                guard let firstOperation = Double(self.firstOperand) else {return}
+                guard let secondOperation =
+                        Double(self.sencondOperand) else {return}
+                switch self.currentOperation{
+                case .Add :
+                    self.result = "\(firstOperation + secondOperation)"
+                default:
+                    break
+                }
+                self.firstOperand = self.result
+                self.numberOutPutLabel.text = self.result
+            }
+        }else {
+            // 연산자 클릭하기 전이니깐 첫번째 피연산자에 값추가
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
+       
+    }
+    
     // TODO [x] - 계산기 초기화
     @IBAction func initializeButton(_ sender: UIButton) {
         self.displayNumber = ""
@@ -57,10 +106,7 @@ class ViewController: UIViewController {
         self.result = ""
     }
     
-    // TODO[x] - 연산 기능
-    @IBAction func calculateButton(_ sender: UIButton) {
-        
-    }
+
     
     // TODO[x] - 점 연산자
     @IBAction func dotButton(_ sender: UIButton) {
